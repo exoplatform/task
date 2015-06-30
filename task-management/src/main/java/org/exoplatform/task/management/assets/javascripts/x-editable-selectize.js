90ddacebe35f2a836cb84e55773d681349865e55
trungjc <trungnt@exoplatform.com>
2015-06-30 17:00:11 +0700

    UI-3829: add tooltip for some icons

--- task-management/src/main/java/org/exoplatform/task/management/assets/javascripts/x-editable-selectize.js
+++ task-management/src/main/java/org/exoplatform/task/management/assets/javascripts/x-editable-selectize.js
@@ -33,7 +33,7 @@ define('x_editable_selectize', ['jquery', 'SHARED/edit_inline_js', 'selectize'],
                 }
                 var html = [];
                 $.each(value, function(index, val) {
-                    var span = '<span href="#" class="label primary">'+ val +'</span>';
+                    var span = '<div  class="label primary">'+ val +'</div>';
                     html.push(span);
                 });
                 var $html = html.join("\n");
@@ -144,7 +144,7 @@ define('x_editable_selectize', ['jquery', 'SHARED/edit_inline_js', 'selectize'],
                         '</li>';
                 },
                 item: function(item, escape) {
-                    return '<div class="label primary" href="#">' + escape(item.text) +'</div>';
+                    return '<div class="label primary">' + escape(item.text) +'</div>';
                 },
                 option_create: function(data, escape) {
                     return '<li class="create"><a href="javascript:void(0)">Add <strong>' + escape(data.input) + '</strong>&hellip;</a></li>';
