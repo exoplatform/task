df1d0f774cea4a31d041007031db7e9d4da2fc37
Nguyen The Tuyen <tuyennt@exoplatform.com>
2015-11-27 13:57:59 +0700

    TA-159 [Due Date] Due date popover does NOT match None, Today, Tomorrow or Next Week when editing Due date

--- task-management/src/main/java/org/exoplatform/task/management/assets/javascripts/x-editable-calendar.js
+++ task-management/src/main/java/org/exoplatform/task/management/assets/javascripts/x-editable-calendar.js
@@ -12,6 +12,39 @@ define('x_editable_calendar', ['SHARED/jquery', 'SHARED/edit_inline_js', 'task_u
                 DPG = $.fn.task_datepicker.DPGlobal;
             }
 
+            var activeClass = 'active';
+            var isNone = function(date) {
+                return date == undefined || !date;
+            };
+            var isToday = function(date) {
+                if (date == undefined || !date) return false;
+                var current = new Date();
+                return (current.getDate() == date.getDate()
+                            && current.getMonth() == date.getMonth()
+                            && current.getFullYear() == date.getFullYear());
+            };
+            var isTomorrow = function(date) {
+                if (date == undefined || !date) return false;
+                var current = new Date();
+                current = new Date(current.getTime() + 86400000);
+
+                return (current.getDate() == date.getDate()
+                    && current.getMonth() == date.getMonth()
+                    && current.getFullYear() == date.getFullYear());
+            };
+            var isNextWeek = function(date, firstDayOfWeek) {
+                if (date == undefined || !date) return false;
+                var current = new Date();
+                var dayOfWeek = current.getDay();
+                var dayDiff = (dayOfWeek - firstDayOfWeek + 7) % 7;
+                var startOfWeek = new Date(current.getTime() - dayDiff*86400000 + 7*86400000);
+                startOfWeek.setHours(0, 0, 0, 0);
+                var endOfWeek = new Date(startOfWeek.getTime() + 7*86400000 - 1);
+
+                return (date.getTime() > startOfWeek.getTime()
+                        && date.getTime() < endOfWeek.getTime());
+            };
+
             var Calendar = function (options) {
                 this.options = options;
                 this.init('calendar', options, Calendar.defaults);
@@ -30,6 +63,7 @@ define('x_editable_calendar', ['SHARED/jquery', 'SHARED/edit_inline_js', 'task_u
                     var _this = this;
                     this.$input.on('click', '[data-date]', function(e) {
                         var $date = $(e.target).closest('[data-date]');
+                        if ($date.hasClass(activeClass)) return;
                         var val = $date.data('date');
                         var nextTime = new Date().getTime();
                         var oneDay = 86400000; // = 24 * 60 * 60 * 1000;
@@ -103,6 +137,19 @@ define('x_editable_calendar', ['SHARED/jquery', 'SHARED/edit_inline_js', 'task_u
                     var $in = this.$input.find('input[name="calendar"]');
                     $in.val(value);
                     uiCalendar.init($in[0], false, this.options.format, '');
+                    var date = value ? uiCalendar.currentDate : false;
+                    var selected = '';
+                    if (isNone(date)) {
+                        selected = 'none';
+                    } else if (isToday(date)) {
+                        selected = 'today';
+                        $('.uiCalendarComponent .highLight.today').addClass('selected');
+                    } else if (isTomorrow(date)) {
+                        selected = 'tomorrow';
+                    } else if (isNextWeek(date, uiCalendar.getFirstDayOfWeek() - 1)) {
+                        selected = 'nextweek';
+                    }
+                    $('[data-date="'+selected+'"]').addClass(activeClass);
                 },
 
                 /**
