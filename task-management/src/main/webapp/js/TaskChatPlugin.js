8ed9147476632d3c1c49f2a36afd83631991fc7a
Tran The Trong <trongtt@exoplatform.com>
2016-10-06 19:41:14 +0700

    Move to use new suggester component

--- task-management/src/main/webapp/js/TaskChatPlugin.js
+++ task-management/src/main/webapp/js/TaskChatPlugin.js
@@ -1,208 +1,236 @@
 (function($) {
-	var createTaskUrl = '';
-	$(".create-task-button")
-			.on(
-					"click",
-					function() {
-						var username = $("#task-add-user").val();
-						var task = $("#task-add-task").val();
-						var dueDate = $("#task-add-date").val();
-						var roomName = chatApplication.targetFullname;
-						var isSpace = false;
-						var isTeam = false;
-						var targetUser = chatApplication.targetUser;
-						if (targetUser.indexOf("space-") > -1) {
-							isSpace = true;
-						} else if (targetUser.indexOf("team-") > -1) {
-							isTeam = true;
-						}
+  var createTaskUrl = '';
+    $('#task-add-user').suggester({
+          type : 'tag',
+          plugins: ['remove_button'],
+          valueField: 'name',
+          labelField: 'fullname',
+          searchField: ['fullname'],
+          sourceProviders: ['exo:task-add-user'],
+          providers: {
+            'exo:task-add-user': function(query, callback) {
+                $.ajax({
+                    type: "GET",
+                    url: '/chatServer/users',
+                    data: {
+                        filter : query,
+                        user : chatApplication.username,
+                        token : chatApplication.token,
+                        dbName : chatApplication.dbName
+                    },
+                    complete: function(jqXHR) {
+                        if(jqXHR.readyState === 4) {
+                            var json = $.parseJSON(jqXHR.responseText)
+                            if (json.users != null) {
+                                callback(json.users);
+                            }
+                        }
+                    }
+                });
+            } 
+          }
+      });
 
-						// Validate empty
-						if (task === $("#task-add-task").attr("data-value")
-								|| task === "" || dueDate === "") {
-							return;
-						}
+  $(".create-task-button").on("click", function() {
+    var username = $("#task-add-user").val();
+    var task = $("#task-add-task").val();
+    var dueDate = $("#task-add-date").val();
+    var roomName = chatApplication.targetFullname;
+    var isSpace = false;
+    var isTeam = false;
+    var targetUser = chatApplication.targetUser;
+    if (targetUser.indexOf("space-") > -1) {
+      isSpace = true;
+    } else if (targetUser.indexOf("team-") > -1) {
+      isTeam = true;
+    }
 
-						// Validate datetime
-						if (!uiMiniCalendar.isDate(dueDate)) {
-							bootbox
-									.alertError(
-											chatBundleData["exoplatform.chat.date.invalid.message"],
-											function(e) {
-												e.stopPropagation();
-												$("#task-add-date").select();
-											});
-							return;
-						}
+    // Validate empty
+    if (task === $("#task-add-task").attr("data-value")
+        || task === "" || dueDate === "") {
+      return;
+    }
 
-						// Get selected users
-						var selectedUsers = "";
-						var selectedFullNames = "";
-						$(".task-user-label").each(function(index) {
-							var name = $(this).attr("data-name");
-							var fullname = $(this).attr("data-fullname");
-							if (index === 0) {
-								selectedUsers = name;
-								selectedFullNames = fullname;
-							} else {
-								selectedUsers += "," + name;
-								selectedFullNames += ", " + fullname;
-							}
-						});
+    // Validate datetime
+    if (!uiMiniCalendar.isDate(dueDate)) {
+      bootbox
+          .alertError(
+              chatBundleData["exoplatform.chat.date.invalid.message"],
+              function(e) {
+                e.stopPropagation();
+                $("#task-add-date").select();
+              });
+      return;
+    }
 
-						if (selectedUsers === "") {
-							if (username !== "") {
-								bootbox
-										.alertError(
-												chatBundleData["exoplatform.chat.task.invalidUser.message"]
-														.replace("{0}",
-																username),
-												function(e) {
-													e.stopPropagation();
-													$("#task-add-user")
-															.select();
-												});
-							}
-							return;
-						}
-						hideMeetingPanel();
-						// Disable button while server updating
-						setActionButtonEnabled('.create-task-button', false);
+    // Get selected users
+    var selectedUsers = "";
+    var selectedFullNames = "";
+    $(".task-user-label").each(function(index) {
+      var name = $(this).attr("data-name");
+      var fullname = $(this).attr("data-fullname");
+      if (index === 0) {
+        selectedUsers = name;
+        selectedFullNames = fullname;
+      } else {
+        selectedUsers += "," + name;
+        selectedFullNames += ", " + fullname;
+      }
+    });
 
-						// Call server
-						$.ajax({
-							url : createTaskUrl,
-							data : {
-								"extension_action" : "createTask",
-								"username" : selectedUsers,
-								"dueDate" : dueDate,
-								"task" : task,
-								"roomName" : roomName,
-								"isSpace" : isSpace,
-								"isTeam": isTeam
-							},
-							success : function(response) {
+    if (selectedUsers === "") {
+      if (username !== "") {
+        bootbox
+            .alertError(
+                chatBundleData["exoplatform.chat.task.invalidUser.message"]
+                    .replace("{0}",
+                        username),
+                function(e) {
+                  e.stopPropagation();
+                  $("#task-add-user")
+                      .select();
+                });
+      }
+      return;
+    }
+    hideMeetingPanel();
+    // Disable button while server updating
+    setActionButtonEnabled('.create-task-button', false);
 
-								var options = {
-									type : "type-task",
-									username : selectedUsers,
-									fullname : selectedFullNames,
-									dueDate : dueDate,
-									task : task
-								};
-								var msg = task;
+    // Call server
+    $.ajax({
+      url : createTaskUrl,
+      data : {
+        "extension_action" : "createTask",
+        "username" : selectedUsers,
+        "dueDate" : dueDate,
+        "task" : task,
+        "roomName" : roomName,
+        "isSpace" : isSpace,
+        "isTeam": isTeam
+      },
+      success : function(response) {
 
-								chatApplication.chatRoom.sendMessage(msg,
-										options, "true");
-								setActionButtonEnabled('.create-task-button',
-										true);
+        var options = {
+          type : "type-task",
+          username : selectedUsers,
+          fullname : selectedFullNames,
+          dueDate : dueDate,
+          task : task
+        };
+        var msg = task;
 
-							},
-							error : function(xhr, status, error) {
-								console.log("error");
-								setActionButtonEnabled('.create-task-button',
-										true);
-							}
-						});
-					});
-	
-	chatApplication.initMention();
-			
-	chatApplication.registerPlugin({
-		'getEvent' : function() {
-			return 'beforeSend';
-		},	
-		'onEvent' : function(context) {
-			var msg = context.msg;
-			
-			var pattern = /\s*\+\+\S+/;
-			if (pattern.test(msg)) {
-				context.continueSend = false;
-				var msg = msg.replace("++", "");
-				var roomName = chatApplication.targetFullname;
-				var isSpace = false, isTeam = false;
-				var targetUser = chatApplication.targetUser;
-				if (targetUser.indexOf("space-") > -1) {
-					isSpace = true;
-				} else if (targetUser.indexOf("team-") > -1) {
-					isTeam = true;
-				}
-				
-				chatApplication.getUsers(targetUser, function (jsonData) {
-					var participants = [];
-					$.each(jsonData.users, function(idx, elem) {
-						participants.push(elem.name);					
-					});
-					
-					// Call server
-					$.ajax({
-						url : createTaskUrl,
-						data : {
-							"extension_action" : "createTaskInline",
-							"task": msg,
-							"isSpace": isSpace,
-							"isTeam": isTeam,
-							"roomName": roomName,
-							"participants": participants.join(",")
-						},
-						success : function(response) {
-							var options = {
-									"type" : "type-task",
-									"username" : response.assignee,
-									"fullname" : response.fullName,
-									"task" : response.title,
-									"dueDate" : response.dueDate
-							};
-							
-							chatApplication.chatRoom.sendMessage(response.title,
-									options, "true");
-							setActionButtonEnabled('.create-task-button',
-									true);
-						},
-						error : function(xhr, status, error) {
-							console.log("fail to create inline task: " + error);
-						}
-					});				
-				});			
-			}
-		}
-	});
-	
-	chatApplication.chatRoom.registerPlugin({
-		"getType" : function() {
-			return "type-task";
-		},
-		"getActionMeetingStyleClasses" : function(options) {
-			var actionType = options.type;
-			var out = "";
+        chatApplication.chatRoom.sendMessage(msg,
+            options, "true");
+        setActionButtonEnabled('.create-task-button',
+            true);
 
-			if ("type-task" === actionType) {
-				out += "                <i class='uiIconChat32x32Task uiIconChat32x32LightGray'></i>";
-			}
-			return out;
-		},
-		"messageBeautifier" : function(objMessage, options) {
-			if (options.type === "type-task") {
-				var out = "";
-				out += "<b>" + options.task + "</b>";
-				out += "<div class='msTimeEvent'>";
-				out += "  <div>";
-				out += "    <i class='uiIconChatAssign uiIconChatLightGray mgR10'></i><span class='muted'>"
-						+ chatBundleData["exoplatform.chat.assign.to"]
-						+ ": </span>" + options.fullname;
-				out += "  </div>";
-				out += "  <div>";
-				out += "    <i class='uiIconChatClock uiIconChatLightGray mgR10'></i><span class='muted'>"
-						+ chatBundleData["exoplatform.chat.due.date"]
-						+ ":</span> <b>" + options.dueDate + "</b>";
-				out += "  </div>";
-				out += "</div>";
-				return out;
-			}
-		}
-	});
-	
-	function setMiniCalendarToDateField(dateFieldId) {
+      },
+      error : function(xhr, status, error) {
+        console.log("error");
+        setActionButtonEnabled('.create-task-button',
+            true);
+      }
+    });
+  });
+  
+  chatApplication.initMention();
+      
+  chatApplication.registerPlugin({
+    'getEvent' : function() {
+      return 'beforeSend';
+    },  
+    'onEvent' : function(context) {
+      var msg = context.msg;
+      
+      var pattern = /\s*\+\+\S+/;
+      if (pattern.test(msg)) {
+        context.continueSend = false;
+        var msg = msg.replace("++", "");
+        var roomName = chatApplication.targetFullname;
+        var isSpace = false, isTeam = false;
+        var targetUser = chatApplication.targetUser;
+        if (targetUser.indexOf("space-") > -1) {
+          isSpace = true;
+        } else if (targetUser.indexOf("team-") > -1) {
+          isTeam = true;
+        }
+        
+        chatApplication.getUsers(targetUser, function (jsonData) {
+          var participants = [];
+          $.each(jsonData.users, function(idx, elem) {
+            participants.push(elem.name);         
+          });
+          
+          // Call server
+          $.ajax({
+            url : createTaskUrl,
+            data : {
+              "extension_action" : "createTaskInline",
+              "task": msg,
+              "isSpace": isSpace,
+              "isTeam": isTeam,
+              "roomName": roomName,
+              "participants": participants.join(",")
+            },
+            success : function(response) {
+              var options = {
+                  "type" : "type-task",
+                  "username" : response.assignee,
+                  "fullname" : response.fullName,
+                  "task" : response.title,
+                  "dueDate" : response.dueDate
+              };
+              
+              chatApplication.chatRoom.sendMessage(response.title,
+                  options, "true");
+              setActionButtonEnabled('.create-task-button',
+                  true);
+            },
+            error : function(xhr, status, error) {
+              console.log("fail to create inline task: " + error);
+            }
+          });       
+        });     
+      }
+    }
+  });
+  
+  chatApplication.chatRoom.registerPlugin({
+    "getType" : function() {
+      return "type-task";
+    },
+    "getActionMeetingStyleClasses" : function(options) {
+      var actionType = options.type;
+      var out = "";
+
+      if ("type-task" === actionType) {
+        out += "                <i class='uiIconChat32x32Task uiIconChat32x32LightGray'></i>";
+      }
+      return out;
+    },
+    "messageBeautifier" : function(objMessage, options) {
+      if (options.type === "type-task") {
+        var out = "";
+        out += "<b>" + options.task + "</b>";
+        out += "<div class='msTimeEvent'>";
+        out += "  <div>";
+        out += "    <i class='uiIconChatAssign uiIconChatLightGray mgR10'></i><span class='muted'>"
+            + chatBundleData["exoplatform.chat.assign.to"]
+            + ": </span>" + options.fullname;
+        out += "  </div>";
+        out += "  <div>";
+        out += "    <i class='uiIconChatClock uiIconChatLightGray mgR10'></i><span class='muted'>"
+            + chatBundleData["exoplatform.chat.due.date"]
+            + ":</span> <b>" + options.dueDate + "</b>";
+        out += "  </div>";
+        out += "</div>";
+        return out;
+      }
+    }
+  });
+  
+  function setMiniCalendarToDateField(dateFieldId) {
     var dateField = document.getElementById(dateFieldId);
     dateField.onfocus=function(){
       uiMiniCalendar.init(this,false,"MM/dd/yyyy","", chatBundleData["exoplatform.chat.monthNames"]);
@@ -219,24 +247,24 @@
   };
   setMiniCalendarToDateField('task-add-date');
 
-	function hideMeetingPanel() {
-		$(".meeting-action-popup").css("display", "none");
-		$(".meeting-action-toggle").removeClass("active");
-	}
+  function hideMeetingPanel() {
+    $(".meeting-action-popup").css("display", "none");
+    $(".meeting-action-toggle").removeClass("active");
+  }
 
-	function setActionButtonEnabled(btnClass, isEnabled) {
-		if (isEnabled) {
-			$(btnClass).css('cursor', "default");
-			$(btnClass).removeAttr('disabled');
-		} else {
-			$(btnClass).css('cursor', "progress");
-			$(btnClass).attr('disabled', 'disabled');
-		}
-	}
+  function setActionButtonEnabled(btnClass, isEnabled) {
+    if (isEnabled) {
+      $(btnClass).css('cursor', "default");
+      $(btnClass).removeAttr('disabled');
+    } else {
+      $(btnClass).css('cursor', "progress");
+      $(btnClass).attr('disabled', 'disabled');
+    }
+  }
 
-	return {
-		setActionUrl : function(url) {
-			createTaskUrl = url;
-		}
-	}
+return {
+  setActionUrl : function(url) {
+    createTaskUrl = url;
+  }
+}
 })($);
