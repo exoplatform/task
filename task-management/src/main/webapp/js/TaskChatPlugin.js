ad1c5f7dbe2c93cbc2932ec0e24eb1db32eda792
hassenelkamel-exo <54801416+hassenelkamel-exo@users.noreply.github.com>
2020-08-03 17:05:24 +0200

    Maint-27055: fix bug due date (#245)

--- task-management/src/main/webapp/js/TaskChatPlugin.js
+++ task-management/src/main/webapp/js/TaskChatPlugin.js
@@ -181,8 +181,8 @@ var chatTaskComposerAppPlugin = {
     });
   },
   processData: function(data, message) {
-    var url = data.url ? data.url : data.length && data.length === 1 && data[0].url ? data[0].url : '';
-    var title = data.title ? data.title : data.length && data.length === 1 && data[0].title ? data[0].title : '';
+    var url = data.url ? data.url : '';
+    var title = data.title ? data.title: '';
     var dueDate = data.dueDate ? data.dueDate.split(" ")[0] : '';
     message.options.url = url;
     message.options.task = decodeURI(title);
