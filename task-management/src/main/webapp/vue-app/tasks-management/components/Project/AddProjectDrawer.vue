456bdb74702c05bafc76e686a05dcc1cb7edb149
saraBoutej <sboutej@exoplatform.com>
2020-09-15 20:43:00 +0100

    Add Project drawer

---
+++ task-management/src/main/webapp/vue-app/tasks-management/components/Project/AddProjectDrawer.vue
@@ -0,0 +1,32 @@
+<template>
+  <exo-drawer
+    ref="AddProjectDrawer"
+    body-classes="hide-scroll decrease-z-index-more"
+    right>
+    <template slot="title">
+      {{ $t('label.addProject') }}
+    </template>
+    <template slot="content">
+      <div class="addProjectTitle d-flex align-center pa-4">
+        <i class="uiIcon uiIconProject"></i>
+        <v-text-field
+          :label="$t('label.label.titleProject')"
+          class="projectInputTitle pl-3 pt-1"
+          single-line
+        ></v-text-field>
+      </div>
+    </template>
+  </exo-drawer>
+</template>
+<script>
+  export default {
+    methods: {
+      open() {
+        this.$refs.AddProjectDrawer.open();
+      },
+      cancel() {
+        this.$refs.AddProjectDrawer.close();
+      },
+    }
+  }
+</script>
