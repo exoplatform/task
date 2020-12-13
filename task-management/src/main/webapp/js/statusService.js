5c3e2857b3ad4fdd68dc8bfbcdee9d79ff4c79ec
Mohamed Amine Krout <mkrout@exoplatform.com>
2020-12-13 01:20:18 +0100

    Project tasks board view 3 dots (#392)
    * TASK-28101 : Project_manager_US01: Project's Tasks BOARD view-3 dots
    
    * TASK-28101 : Project_manager_US01: Project's Tasks BOARD view-3 dots

---
+++ task-management/src/main/webapp/js/statusService.js
@@ -0,0 +1,31 @@
+import { tasksConstants } from "./tasksConstants";
+
+
+export function createStatus(status) {
+    return fetch(`${tasksConstants.PORTAL}/${tasksConstants.PORTAL_REST}/status`, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+        method: 'POST',
+        body: JSON.stringify(status)
+    }).then(resp => resp.json());
+}
+
+export function updateStatus(status) {
+    return fetch(`${tasksConstants.PORTAL}/${tasksConstants.PORTAL_REST}/status/${status.id}`, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+        method: 'PUT',
+        body: JSON.stringify(status)
+    }).then(resp => resp.json());
+}
+
+export function deleteStatus(statusId) {
+    return fetch(`${tasksConstants.PORTAL}/${tasksConstants.PORTAL_REST}/status/${statusId}`, {
+        credentials: 'include',
+        method: 'DELETE',
+    })
+}
