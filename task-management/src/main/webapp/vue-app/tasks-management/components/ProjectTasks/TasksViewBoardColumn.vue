d324b610f3829a1341e7c48cf2cc49fb2c781b68
Mohamed Amine Krout <mkrout@exoplatform.com>
2021-01-25 13:02:39 +0100

    Us drag drop 04 (#168)
     Task-36933: US_DragDrop_04: Add grey highlight effect on-destination when D&Dgh

--- task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TasksViewBoardColumn.vue
+++ task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TasksViewBoardColumn.vue
@@ -15,8 +15,8 @@
     <draggable 
       v-model="tasksList" 
       :move="checkMove"
-      group="people" 
-      class="draggable-palceholder" 
+      group="people"
+      class="draggable-palceholder"
       @start="drag=true"
       @end="drag=false">
       <task-view-card
@@ -59,20 +59,21 @@
       drag(val) {
         if(!val&&this.task&&this.newStatus&&this.task.status.id !== this.newStatus){
              this.$emit('updateTaskStatus', this.task,this.newStatus);
-        }
-      },
+             document.getElementsByClassName("taskBoardColumn").forEach(element => element.style.backgroundColor= "#FFFFFF");
+        }},
    },
     methods: {
       updateTaskCompleted(e){
         this.$emit('updateTaskCompleted', e);
       },
       checkMove(evt){
-         if(evt){
-      this.task = evt.draggedContext.element.task
-      this.newStatus = evt.to.parentElement.id
-       } 
-      
-    },
+        if(evt){
+          document.getElementsByClassName("taskBoardColumn").forEach(element => element.style.backgroundColor= "#FFFFFF");
+          evt.to.parentElement.getElementsByClassName("taskBoardColumn").forEach(element => element.style.backgroundColor= "#f2f2f2");
+          this.task = evt.draggedContext.element.task
+          this.newStatus = evt.to.parentElement.id;
+        }
+      },
     deleteStatus(status) {
           this.$emit('delete-status', status);
       },
@@ -93,3 +94,36 @@
     }
 
 </script>
+<style>
+  @keyframes beginDrag {
+    from {
+      transform: rotate(0deg)
+    }
+    10% {
+      -webkit-transform: rotate(-12deg);
+      transform: rotate(-12deg)
+    }
+    30% {
+      -webkit-transform: rotate(8deg);
+      transform: rotate(8deg)
+    }
+    55% {
+      -webkit-transform: rotate(-5deg);
+      transform: rotate(-5deg)
+    }
+    80% {
+      -webkit-transform: rotate(3deg);
+      transform: rotate(3deg)
+    }
+    to {
+      -webkit-transform: rotate(-1deg);
+      transform: rotate(-1deg)
+    }
+  }
+  .ghost-card {
+    opacity: 0.5;
+    background: #F7FAFC;
+    border: 1px solid #578dc9;
+    animation: beginDrag 1s ease forwards;
+  }
+</style>
