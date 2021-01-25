04a63162bed48325a980af21bc3caf39bd0af169
aelbenney <42932906+aelbenney@users.noreply.github.com>
2021-01-25 10:27:41 +0100

    TASK-39581: Restrict mentioning users in task comment to only project participants (#161)

--- task-management/src/main/webapp/vue-app/taskDrawer/components/TaskDrawerComponents/TaskCommentEditor.vue
+++ task-management/src/main/webapp/vue-app/taskDrawer/components/TaskDrawerComponents/TaskCommentEditor.vue
@@ -28,6 +28,10 @@
         type: String,
         default: ''
       },
+      task: {
+        type: Object,
+        default: null
+      },
       reset: {
         type: Boolean,
         default: false
@@ -60,14 +64,16 @@
     },
     mounted() {
       this.initCKEditor();
+      const thiss = this;
       $('body').suggester('addProvider', 'task:people', function (query, callback) {
         const _this = this;
-        findUsersToMention(query).then((data) => {
+        const projectId = thiss.task.status ? thiss.task.status.project.id : null;
+        findUsersToMention(projectId, query).then((data) => {
           const result = [];
           for (let i = 0; i < data.length; i++) {
             const d = data[i];
             const item = {
-              uid: d.id.substr(1),
+              uid: d.id,
               name: d.name,
               avatar: d.avatar
             };
