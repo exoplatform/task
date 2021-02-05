4dfc168ce951c29a2874a6becd9577dd85b46c1e
mkrout <mkrout@exoplatform.com>
2021-02-05 12:33:28 +0100

    Task-39386: [NEWF] US_AlertMessages_01: Alert messages for Tasks management

--- task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TaskViewListItem.vue
+++ task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TaskViewListItem.vue
@@ -188,6 +188,11 @@
 
       if (typeof task.id !== 'undefined') {
         return this.$tasksService.updateCompleted(task).then(task => {
+                      if(task.completed){
+              this.$root.$emit('show-alert', {type: 'success',message: this.$t('alert.success.task.completed')});   
+            }else{
+              this.$root.$emit('show-alert', {type: 'success',message: this.$t('alert.success.task.unCompleted')});
+            }
           this.$emit('update-task-completed', task);
           if( task.completed === true && !this.showCompletedTasks) {
             this.removeCompletedTask = true;
@@ -195,7 +200,10 @@
         }).then(this.task.task.completed = task.showCompleteTasks)
                 .catch(e => {
                   console.debug("Error updating project", e);
-                  this.$emit('error', e && e.message ? e.message : String(e));
+                  this.$root.$emit('show-alert', {
+                    type: 'error',
+                    message: this.$t('alert.error')
+                });
                   this.postProject = false;
                 });
       }
