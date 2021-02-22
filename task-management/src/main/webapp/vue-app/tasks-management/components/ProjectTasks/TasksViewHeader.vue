23b7f002d298b62c98e9492722bcdbc4dfbe8c6f
Rdidmohamedelhadi <54801286+Rdidmohamedelhadi@users.noreply.github.com>
2021-02-22 10:30:04 +0100

    Task-40507 : Add I18n to tasks column status (#298)

--- task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TasksViewHeader.vue
+++ task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TasksViewHeader.vue
@@ -2,36 +2,22 @@
   <div
     :id="'task-'+viewType+'-'+status.id"
     class="tasksViewHeader d-flex justify-space-between align-center">
-    <input 
-      v-if="editStatus || status.edit"      
-      ref="autoFocusInput1" 
-      v-model="status.name" 
+    <input
+      v-if="editStatus || status.edit"
+      ref="autoFocusInput1"
+      v-model="status.name"
       placeholder="Status Name"
       type="text"
       class="taskStatusName font-weight-bold text-color mb-1"
-      required  
+      required
       autofocus
-      @keyup="checkImput($event,index)">     
-    <div 
+      @keyup="checkImput($event,index)">
+    <div
       v-else
-      class="taskStatusName font-weight-bold text-color mb-1" 
-      @click="editStatus = true">{{ status.name }}</div>   
+      class="taskStatusName font-weight-bold text-color mb-1"
+      @click="editStatus = true">{{ getI18N(status.name) }}</div>
     <div class="taskNumberAndActions d-flex align-center mb-1">
       <span class="caption">{{ tasksNumber }}</span>
-      <!-- <span v-if="tasksNumber < maxTasksToShow" class="caption">{{ tasksNumber }}</span>
-      <div v-else class="showTasksPagination">
-        <span class="caption">
-          {{ limitTasksToshow }} - {{ initialTasksToShow }} of {{ tasksNumber }}
-        </span>
-        <v-btn
-          :disabled="disableBtnLoadMore"
-          icon
-          small
-          @click="loadNextTasks">
-          <i class="uiIcon uiIconArrowNext text-color"></i>
-        </v-btn>
-
-      </div> -->
       <i
         icon
         small
@@ -51,27 +37,27 @@
               <span>{{ $t('label.addTask') }}</span>
             </v-list-item-title>
           </v-list-item>
-          <v-list-item 
-            v-if="project.canManage" 
-            class="menu-list" 
+          <v-list-item
+            v-if="project.canManage"
+            class="menu-list"
             @click="addColumn(index)" >
             <v-list-item-title class="subtitle-2">
               <i class="uiIcon uiIconRotateLeft pr-1"></i>
               <span>{{ $t('label.status.before') }}</span>
             </v-list-item-title>
           </v-list-item>
-          <v-list-item 
-            v-if="project.canManage" 
-            class="menu-list" 
+          <v-list-item
+            v-if="project.canManage"
+            class="menu-list"
             @click="addColumn(index+1)" >
             <v-list-item-title class="subtitle-2">
               <i class="uiIcon uiIconRotateRight pr-1"></i>
               <span> {{ $t('label.status.after') }} </span>
             </v-list-item-title>
           </v-list-item>
-          <v-list-item 
-            v-if="project.canManage" 
-            class="menu-list" 
+          <v-list-item
+            v-if="project.canManage"
+            class="menu-list"
             @click="deleteStatus()" >
             <v-list-item-title class="subtitle-2">
               <i class="uiIcon uiIconDelete pr-1"></i>
@@ -122,14 +108,6 @@
         disableBtnLoadMore: false,
       }
     },
-    computed: {
-      initialTasksToShow() {
-        return this.originalTasksToShow;
-      },
-      limitTasksToshow() {
-        return this.tasksStatsStartValue;
-      }
-    },
     created() {
       $(document).on('mousedown', () => {
         if (this.displayActionMenu) {
@@ -137,24 +115,9 @@
             this.displayActionMenu = false;
           }, 200);
         }
-/*         if (this.editStatus || this.status.edit) {
-          window.setTimeout(() => {
-            this.cancelAddColumn(this.index);
-          }, 200);
-        } */
       });
     },
     methods: {
-      loadNextTasks() {
-        if(this.tasksNumber - this.originalTasksToShow >= this.maxTasksToShow) {
-          this.tasksStatsStartValue = this.originalTasksToShow+1;
-          this.originalTasksToShow += this.maxTasksToShow;
-        } else {
-          this.tasksStatsStartValue = this.originalTasksToShow;
-          this.originalTasksToShow = this.tasksNumber;
-          this.disableBtnLoadMore = true;
-        }
-      },
       openTaskDrawer() {
         const defaultTask= {id:null,
         status:this.status,
@@ -166,12 +129,12 @@
       checkImput: function(e,index) {
       if (e.keyCode === 13) {
         this.saveStatus(index)
-      } 
+      }
       if (e.keyCode === 27) {
         this.cancelAddColumn(index)
-      }      
+      }
     },
-    
+
       deleteStatus() {
           this.$emit('delete-status', this.status);
       },
@@ -185,8 +148,8 @@
           this.editStatus=false
         }
       },
-      addColumn(index) {       
-          this.$emit('add-column',index);    
+      addColumn(index) {
+          this.$emit('add-column',index);
       },
       cancelAddColumn(index) {
         if(this.status.id){
@@ -195,6 +158,11 @@
         }else{
           this.$emit('cancel-add-column',index);
         }
+      },
+      getI18N(label){
+        const fieldLabelI18NKey = `tasks.status.${label}`;
+        const fieldLabelI18NValue = this.$t(fieldLabelI18NKey);
+        return  fieldLabelI18NValue === fieldLabelI18NKey ? label : fieldLabelI18NValue;
       }
     }
   }
