c810e222ce11d6df2d4f12748a162f16b7f692e5
saraBoutej <sboutej@exoplatform.com>
2021-03-16 15:49:03 +0100

    Put Task Drawer in a dedicated module

---
+++ task-management/src/main/webapp/vue-app/taskDrawer/main.js
@@ -0,0 +1,13 @@
+import './initComponents.js';
+
+// get overrided components if exists
+if (extensionRegistry) {
+  const components = extensionRegistry.loadComponents('TaskDrawer');
+  if (components && components.length > 0) {
+    components.forEach(cmp => {
+      Vue.component(cmp.componentName, cmp.componentOptions);
+    });
+  }
+}
+
+Vue.use(Vuetify);
