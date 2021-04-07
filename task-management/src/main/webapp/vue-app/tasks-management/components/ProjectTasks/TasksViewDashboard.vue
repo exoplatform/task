446fead66424ddafbab9dbe008bc76a1ddcc1ace
Lotfi Ben Gayed <lbengayed@exoplatform.com>
2021-04-07 15:35:52 +0100

    Task-42803:US_EmptyBoardListUX_01: Improve Empty Board and List views (#461)

--- task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TasksViewDashboard.vue
+++ task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TasksViewDashboard.vue
@@ -129,7 +129,7 @@
     </div>
     <v-tabs-items
       v-show="!filterProjectActive"
-      v-if="tasksList && tasksList.length && !loadingTasks"
+      v-if="!loadingTasks"
       :key="id">
       <div
         v-show="taskViewTabName == 'board'"
@@ -154,7 +154,7 @@
           :status-list="statusList"
           :tasks-list="tasksList"
           :filter-task-completed="filterAsCompleted"
-          :filter-by-status="filterByStatus"
+          :filter-by-status="filterByStatus=true"
           @update-status="updateStatus" />
       </div>
       <v-tab-item
@@ -164,12 +164,7 @@
           :tasks-list="tasksList" />
       </v-tab-item>
     </v-tabs-items>
-    <div
-      v-if="(!tasksList || !tasksList.length) && !loadingTasks"
-      class="noTasksProject">
-      <div class="noTasksProjectIcon"><i class="uiIcon uiIconTask"></i></div>
-      <div class="noTasksProjectLabel"><span>{{ $t('label.noTasks') }}</span></div>
-    </div>
+
     <div class="ma-0 border-box-sizing">
       <v-btn
         v-if="loadingTasks"
