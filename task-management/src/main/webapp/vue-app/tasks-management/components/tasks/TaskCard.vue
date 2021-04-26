399b33950053c1ad245d7eebc0bd21275d1304fc
Lotfi Ben Gayed <lbengayed@exoplatform.com>
2021-04-26 10:51:00 +0100

    Task-43145: Fix multiple bugs on mobile view (#521)
    * Task-43145:Multiple bugs on mobile view

--- task-management/src/main/webapp/vue-app/tasks-management/components/tasks/TaskCard.vue
+++ task-management/src/main/webapp/vue-app/tasks-management/components/tasks/TaskCard.vue
@@ -1,94 +1,111 @@
 <template>
-  <v-app id="taskCardItem" class="pa-3">
+  <v-app
+    id="taskCardItem"
+    :class="removeCompletedTask && 'completedTask' || ''">
     <v-card
       :class="[getTaskPriorityColor(task.task.priority)]"
-      class="taskCard pa-3"
+      class="taskCard taskViewCard pa-2"
       flat>
-      <div class="taskTitleId d-flex justify-space-between">
-        <div class="taskTitle d-flex align-start">
-          <div class="taskCheckBox" @click="updateCompleted">
-            <v-switch
-              ref="autoFocusInput2"
-              class="d-none"
-              true-value="true"
-              false-value="false" />
-            <i :title="$t(getTaskCompletedTitle())" :class="getTaskCompleted()"></i>
-          </div>
+      <div class="taskTitleId  d-flex justify-space-between">
+        <div class="taskCheckBox">
+          <v-switch
+            ref="autoFocusInput2"
+            class="d-none"
+            true-value="true"
+            false-value="false" />
+          <i
+            :title="$t(getTaskCompletedTitle())"
+            :class="getTaskCompleted()"
+            @click="updateCompleted"></i>
+        </div>
+        <div class="taskTitle d-flex align-start" @click="openTaskDrawer()">
           <a
             ref="tooltip"
-            :class="getTitleTaskClass()">
-            <ellipsis
-              v-if="task.task.title "
-              :title="task.task.title "
-              :data="task.task.title "
-              :line-clamp="2"
-              end-char=".." />
+            :class="getTitleTaskClass()"
+            :title="task.task.title"
+            class="taskCardViewTitle">
+            <span class="taskTitleEllipsis">{{ task.task.title }}</span>
           </a>
         </div>
-        <div 
-          class="taskId"
-          @click="openTaskDrawer()">
-          <span class="caption text-sub-title">ID : {{ task.task.id }}</span>
-        </div>
       </div>
-      <div class="taskAssigneeAndLabels d-flex justify-space-between align-center mt-3">
-        <div class="taskAssignee d-flex flex-nowrap">
-          <exo-user-avatar
-            v-for="userAvatar in avatarToDisplay"
-            :key="userAvatar"
-            :username="userAvatar.username"
-            :title="userAvatar.displayName"
-            :avatar-url="userAvatar.avatar"
-            :size="iconSize"
-            :style="'background-image: url('+userAvatar.avatar+')'"
-            class="mx-1 taskWorkerAvatar" />
-          <div class="seeMoreAvatars">
+
+      <v-divider v-if="displayCardBottomSection" />
+      <div
+        v-if="displayCardBottomSection"
+        class="taskActionsAndDate d-flex align-center justify-space-between">
+        <div
+          class="taskActionsAndLabels d-flex align-center">
+          <div
+            v-if="assigneeAndCoworkerArray && assigneeAndCoworkerArray.length"
+            class="taskWorker  justify-space-between pe-2">
             <div
-              v-if="assigneeAndCoworkerArray.length > maxAvatarToShow"
-              class="seeMoreItem"
-              @click="$root.$emit('displayTasksAssigneeAndCoworker', assigneeAndCoworkerArray)">
-              <v-avatar
-                :size="iconSize">
-                <img
-                  :src="assigneeAndCoworkerArray[maxAvatarToShow].avatar"
-                  :title="assigneeAndCoworkerArray[maxAvatarToShow].displayName">
-              </v-avatar>
-              <span class="seeMoreAvatarList">+{{ showMoreAvatarsNumber }}</span>
+              :class="assigneeAndCoworkerArray && !assigneeAndCoworkerArray.length && task && task.labels && !task.labels.length && 'hideTaskAssignee'"
+              class="taskAssignee d-flex flex-nowrap">
+              <exo-user-avatar
+                v-for="userAvatar in avatarToDisplay"
+                :key="userAvatar"
+                :username="userAvatar.username"
+                :title="userAvatar.displayName"
+                :avatar-url="userAvatar.avatar"
+                :size="iconSize"
+                :style="'background-image: url('+userAvatar.avatar+')'"
+                class="mx-1 taskWorkerAvatar" />
+              <div class="seeMoreAvatars">
+                <div
+                  v-if="assigneeAndCoworkerArray.length > maxAvatarToShow"
+                  class="seeMoreItem"
+                  :title="getAssigneeAndCoworkerList(assigneeAndCoworkerArray)"
+                  @click="$root.$emit('displayTasksAssigneeAndCoworker', assigneeAndCoworkerArray)">
+                  <v-avatar
+                    :size="iconSize">
+                    <img
+                      :src="assigneeAndCoworkerArray[maxAvatarToShow].avatar"
+                      :title="assigneeAndCoworkerArray[maxAvatarToShow].displayName">
+                  </v-avatar>
+                  <span class="seeMoreAvatarList">+{{ showMoreAvatarsNumber }}</span>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <v-divider v-if="task && commentCount || task && task.labels && task.labels.length || taskDueDate" />
-      <div 
-        v-if="task && commentCount || task && task.labels && task.labels.length || taskDueDate"
-        class="taskStatusAndDate d-flex justify-space-between pt-3" 
-        @click="openTaskDrawer()">
-        <div class="taskActionsAndLabels d-flex align-center">
-          <div v-if="task.commentCount" class="taskComment d-flex pe-2">
+          <div
+            v-if="task.commentCount"
+            class="taskComment d-flex pe-2"
+            @click="openTaskDrawer()">
             <i class="uiIcon uiCommentIcon"></i>
             <span class="taskCommentNumber caption">{{ task.commentCount }}</span>
           </div>
-          <div v-if="task.labels && task.labels.length" class="taskLabels ">
+          <div
+            v-if="task.labels && task.labels.length"
+            :class="getClassLabels()"
+            @click="openTaskDrawer()">
             <v-chip
               v-if="task.labels && task.labels.length == 1"
               :color="task.labels[0].color"
-              class="mx-1 white--text font-weight-bold"
+              :title="task.labels[0].name"
+              class="mx-1 font-weight-bold theme--light"
               label
               small>
-              {{ task.labels[0].name }}
+              <span class="text-truncate">
+                {{ task.labels[0].name }}
+              </span>
             </v-chip>
             <div
               v-else-if="task.labels && task.labels.length > 1"
               :title="getLabelsList(task.labels)"
-              class="taskTags d-flex">
+              class="taskTags d-flex theme--light">
               <i class="uiIcon uiTagIcon"></i>
               <span class="taskAttachNumber caption">{{ task.labels.length }}</span>
             </div>
           </div>
         </div>
-        <div class="taskDueDate">
-          <div v-if="taskDueDate">
-            <date-format :value="taskDueDate" :format="dateTimeFormat" />
+        <div
+          v-if="taskDueDate"
+          class="taskStatusAndDate"
+          @click="openTaskDrawer()">
+          <div class="taskDueDate" :class="getOverdueTask(taskDueDate) ? 'red--text' : ''">
+            <div>
+              <date-format :value="taskDueDate" :format="dateTimeFormat" />
+            </div>
           </div>
         </div>
       </div>
@@ -101,11 +118,14 @@ export default {
     task: {
       type: Object,
       default: () => ({}),
+    },
+    showCompletedTasks: {
+      type: Boolean,
+      default: false
     }
   },
   data() {
     return {
-      enabled: false,
       user: {},
       iconSize: 26,
       dateTimeFormat: {
@@ -114,10 +134,11 @@ export default {
         day: 'numeric',
       },
       assigneeAndCoworkerArray: [],
-      isPersonnalTask: this.task.status === null,
-      isSpaceProject: this.task.space !== null,
-      maxAvatarToShow: 3,
-      showCompleteTasks: false
+      isPersonnalTask: this.task.task.status === null,
+      drawer: null,
+      maxAvatarToShow: 1,
+      showCompleteTasks: false,
+      removeCompletedTask: false
     };
   },
   computed: {
@@ -134,25 +155,96 @@ export default {
     },
     showMoreAvatarsNumber() {
       return this.assigneeAndCoworkerArray.length - this.maxAvatarToShow;
+    },
+    displayCardBottomSection() {
+      return this.taskDueDate || (this.task.labels && this.task.labels.length) || (this.assigneeAndCoworkerArray && this.assigneeAndCoworkerArray.length) || this.task.commentCount;
     }
   },
-  methods: {
-    getTaskColor() {
-      if (this.task.task.status){
-
-        return this.task.task.status.project.color ? this.task.task.status.project.color : 'noProjectColor' ;
+  created() {
+    this.$root.$on('update-completed-task',(value,id)=>{
+      if (this.task.id === id){
+        this.task.task.completed=value;
+        if (this.task.task.completed === true && !this.showCompletedTasks){
+          this.$emit('update-task-completed', this.task.task);
+          this.removeCompletedTask = true;
+        }
       }
-      else {
-        return null;
+    });
+    this.$root.$on('update-task-assignee',(value,id)=>{
+      this.updateTaskAssignee(value,id);
+    });
+    this.$root.$on('update-remove-task-labels',(value,id)=>{
+      this.updateRemoveTaskLabels(value,id);
+    });
+    this.$root.$on('update-task-labels',(value,id)=>{
+      this.updateTaskLabels(value,id);
+    });
+    this.$root.$on('update-task-comments',(value,id)=>{
+      this.updateTaskComments(value,id);
+    });
+    this.$root.$on('update-task-coworker',(value,id)=>{
+      this.updateTaskCoworker(value,id);
+    });
+    this.getTaskAssigneeAndCoworkers();
+  },
+  methods: {
+    updateRemoveTaskLabels(value,id){
+      if (this.task.id === id){
+        this.task.labels=this.task.labels.filter(item => item.id !== value);
       }
     },
-    getNameProject(){
-      if (this.task.task.status){
-
-        return  this.task.task.status.project.name  ;
+    updateTaskLabels(value,id){
+      if (this.task.id === id && !this.task.labels.includes(value)){
+        this.task.labels.push(value);
       }
-      else {
-        return null;
+    },
+    updateTaskComments(value,id){
+      if (this.task.id === id){
+        this.task.commentCount=value;
+      }
+    },
+    updateTaskCoworker(value,id){
+      if (this.task.id === id){
+        if (value && value.length) {
+          this.task.coworker=[];
+          value.forEach((coworker) => {
+            this.$identityService.getIdentityByProviderIdAndRemoteId('organization',coworker).then(user => {
+              const taskCoworker = {
+                id: `organization:${user.remoteId}`,
+                username: user.remoteId,
+                providerId: 'organization',
+                displayName: user.profile.fullname,
+                avatar: user.profile.avatar,
+              };
+              if (taskCoworker && !this.task.coworker.includes(taskCoworker)){
+                this.task.coworker.push(taskCoworker);
+              }
+            });
+          }).then(() => {
+            this.getTaskAssigneeAndCoworkers();
+          });
+        } else {
+          this.task.coworker = [];
+        }
+      }
+    },
+    updateTaskAssignee(value,id){
+      if (this.task.id === id){
+        if (value===null){
+          this.task.assignee = null;
+        } else {
+          this.$identityService.getIdentityByProviderIdAndRemoteId('organization',value).then(user => {
+            this.task.assignee = {
+              id: `organization:${user.remoteId}`,
+              username: user.remoteId,
+              providerId: 'organization',
+              displayName: user.profile.fullname,
+              avatar: user.profile.avatar,
+            };})
+            .then(() => {
+              this.getTaskAssigneeAndCoworkers();
+            });
+        }
       }
     },
     getTaskPriorityColor(priority) {
@@ -167,29 +259,14 @@ export default {
         return 'taskNonePriority';
       }
     },
-    getTaskStatusLabel(status) {
-      switch (status) {
-      case 'ToDo':
-        return this.$t('exo.tasks.status.todo');
-      case 'InProgress':
-        return this.$t('exo.tasks.status.inprogress');
-      case 'WaitingOn':
-        return this.$t('exo.tasks.status.waitingon');
-      case 'Done':
-        return this.$t('exo.tasks.status.done');
-      default:
-        return status;
-      }
-    },
     getTaskAssigneeAndCoworkers() {
       this.assigneeAndCoworkerArray=[];
-      if (this.task.assignee){
+      if (this.task.assignee && !this.assigneeAndCoworkerArray.includes(this.task.assignee)) {
         this.assigneeAndCoworkerArray.push(this.task.assignee);
-      } 
-      if (this.task.coworker || this.task.coworker.length > 0 )
-      {
+      }
+      if (this.task.coworker || this.task.coworker.length > 0) {
         this.task.coworker.forEach((coworker) => {
-          if (coworker){
+          if (coworker && !this.assigneeAndCoworkerArray.includes(coworker)){
             this.assigneeAndCoworkerArray.push(coworker);
           }
         });
@@ -204,37 +281,54 @@ export default {
         return labelText;
       }
     },
+    getAssigneeAndCoworkerList(assigneeAndCoworker) {
+      if (assigneeAndCoworker.length > 1) {
+        let assigneeAndCoworkerText = '';
+        assigneeAndCoworker.forEach((label) => {
+          assigneeAndCoworkerText += `${label.displayName}\r\n`;
+        });
+        return assigneeAndCoworkerText;
+      }
+    },
     openTaskDrawer() {
       this.$root.$emit('open-task-drawer', this.task.task);
     },
     onCloseDrawer: function(drawer){
       this.drawer = drawer;
     },
-    spaceUrl(spaceUrl) {
-      if (!this.spaceUrl) {
-        return '#';
+    getTaskCompleted() {
+      if (this.task.task.completed === true) {
+        return 'uiIconValidate';
+      } else {
+        return 'uiIconCircle';
       }
-      return `${eXo.env.portal.context}/g/:spaces:${spaceUrl}/`;
+    },
+    getTaskCompletedTitle() {
+      if (this.task.task.completed === true) {
+        return 'message.markAsUnCompleted';
+      } else {
+        return 'message.markAsCompleted';
+      }
     },
     updateCompleted() {
-
       const task = {
         id: this.task.task.id,
         showCompleteTasks: this.showCompleted(),
       };
-
-
       if (typeof task.id !== 'undefined') {
         return this.$tasksService.updateCompleted(task).then(task => {
           if (task.completed){
-            this.$root.$emit('show-alert', {type: 'success',message: this.$t('alert.success.task.completed')});   
+            this.$root.$emit('show-alert', {type: 'success',message: this.$t('alert.success.task.completed')});
           } else {
             this.$root.$emit('show-alert', {type: 'success',message: this.$t('alert.success.task.unCompleted')});
           }
-          this.$emit('update-cart', task);
+          this.$root.$emit('update-task-completed', task);
+          if ( task.completed === true && !this.showCompletedTasks) {
+            this.removeCompletedTask = true;
+          }
         }).then(this.task.task.completed = task.showCompleteTasks)
           .catch(e => {
-            console.error('Error updating project', e);
+            console.error('Error updating task completed', e);
             this.$root.$emit('show-alert', {
               type: 'error',
               message: this.$t('alert.error')
@@ -242,40 +336,56 @@ export default {
             this.postProject = false;
           });
       }
-
-
     },
-    getTaskCompleted() {
-      if (this.task.task.completed===true){
-        return 'uiIconValidate';
+    showCompleted() {
+      if (this.getTaskCompleted() === 'uiIconValidate') {
+        this.showCompleteTasks = false;
+      } else {
+        this.showCompleteTasks = true;
       }
-      else {
-        return 'uiIconCircle';
-      }
+      return this.showCompleteTasks;
     },
     getTitleTaskClass() {
-      if (this.task.task.completed===true){
+      if (this.task.task.completed === true) {
         return 'text-color strikethrough';
-      }
-      else {
+      } else {
         return 'text-color';
       }
     },
-    getTaskCompletedTitle() {
-      if (this.task.task.completed===true){
-        return 'message.markAsUnCompleted';
-      }
-      else {
-        return 'message.markAsCompleted';
+    dateFormatter(dueDate) {
+      if (dueDate) {
+        const date = new Date(dueDate);
+        const day = date.getDate();
+        const month = date.getMonth()+1;
+        const year = date.getFullYear();
+        const formattedTime = `${  year}-${  month  }-${day  }`;
+        return formattedTime;
       }
     },
-    showCompleted(){
-      if (this.getTaskCompleted()==='uiIconValidate'){
-        this.showCompleteTasks=false;
-      } else {
-        this.showCompleteTasks=true;
+    getOverdueTask(value){
+      const Today = new Date();
+      const formattedTimeToday = `${  Today.getFullYear()}-${  Today.getMonth()+1  }-${Today.getDate()  }`;
+      const date = this.dateFormatter(value);
+      if (date===formattedTimeToday){
+        return false;
       }
-      return this.showCompleteTasks;
+      else if (new Date(value) < new Date().getTime()){
+        return true;
+      } else {
+        return false;
+      }
+    },
+    getClassLabels(){
+      if (this.task && this.task.labels && this.task.labels.length === 1){
+        if (this.assigneeAndCoworkerArray && this.assigneeAndCoworkerArray.length && this.task.commentCount){
+          return 'taskLabelsAssigneeComment';
+        } else if ((this.assigneeAndCoworkerArray && this.assigneeAndCoworkerArray.length && !this.task.commentCount)
+            || (!this.assigneeAndCoworkerArray && !this.assigneeAndCoworkerArray.length && this.task.commentCount)){
+          return 'taskLabelsAssignee';
+        } else {
+          return 'taskLabels';
+        }
+      }
     },
   }
 };
