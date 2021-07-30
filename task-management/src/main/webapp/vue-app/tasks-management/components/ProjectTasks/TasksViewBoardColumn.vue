d6903d0548f3c28a4c69d8ec3ddf9b6aa8215f36
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2021-07-30 07:32:00 +0100

    Task-45628 Enhance draggable cards in tasks view board (#586)

--- task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TasksViewBoardColumn.vue
+++ task-management/src/main/webapp/vue-app/tasks-management/components/ProjectTasks/TasksViewBoardColumn.vue
@@ -1,4 +1,4 @@
-<template>
++<template>
   <div :id="status.id">
     <tasks-view-header
       :status="status"
@@ -15,38 +15,35 @@
       @open-quick-add="quickAddTask1=true"
       @add-column="addColumn" />
     <v-divider />
-    <div @mousedown="cancelDrag">
-      <quick-add-card
-        :status="status"
-        :quick-add-task="quickAddTask1"
-        :task-title="taskTitle1"
-        class="status-add-task" 
-        @close-quick-form="quickAddTask1=false" />
-      <draggable 
-        v-model="tasksList"
-        :move="checkMove"
-        :animation="200"
-        :key="draggableKey"
-        ghost-class="ghost-card"
-        class="draggable-palceholder taskBoardColumn"
-        handle=".taskBoardCardItem"
-        :group="{ name: 'status' }"
-        :class="filterNoActive && 'taskBoardNoFilterColumn'"
-        @start="drag=true"
-        @end="drag=false">
-        <task-view-card
-          :id="idViewCard"
-          v-for="taskItem in tasksList"
-          :key="taskItem.task.id"
-          :task="taskItem"
-          :show-completed-tasks="showCompletedTasks"
-          @update-task-completed="updateTaskCompleted" />
-
-        <quick-add-card 
-          :status="status" 
-          :quick-add-task="quickAddTask"
-          :task-title="taskTitle"
-          @close-quick-form="quickAddTask=false" />
+    <quick-add-card 
+      :status="status"
+      :quick-add-task="quickAddTask1"
+      :task-title="taskTitle1"
+      class="status-add-task" 
+      @close-quick-form="quickAddTask1=false" />
+    <draggable 
+      v-model="tasksList" 
+      :move="checkMove"
+      :animation="200"
+      ghost-class="ghost-card"
+      class="draggable-palceholder taskBoardColumn"
+      handle=".taskBoardCardItem"
+      :group="{ name: 'status' }"
+      :class="filterNoActive && 'taskBoardNoFilterColumn'"
+      @start="drag=true"
+      @end="drag=false">
+      <task-view-card
+        v-for="taskItem in tasksList"
+        :key="taskItem.task.id"
+        :task="taskItem"
+        :show-completed-tasks="showCompletedTasks"
+        @update-task-completed="updateTaskCompleted" />
+    
+      <quick-add-card 
+        :status="status" 
+        :quick-add-task="quickAddTask"
+        :task-title="taskTitle"
+        @close-quick-form="quickAddTask=false" />
 
         <v-btn 
           v-if="!quickAddTask"
