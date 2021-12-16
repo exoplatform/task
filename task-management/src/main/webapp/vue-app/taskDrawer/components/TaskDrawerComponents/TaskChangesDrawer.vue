4244b9727960dcb1fb8d1df7db20534ade5df6bf
Montassar-Hamadi <92787967+Montassar-Hamadi@users.noreply.github.com>
2021-12-16 18:04:41 +0100

    Task-43333: Regression - bad display(alignment-UI glitches) of task changes in change history. (#723)
    Problem: a bad alignment of the task changes in task's changes tab is displayed.
    Fix: Task changes should be well displayed with aligned.

--- task-management/src/main/webapp/vue-app/taskDrawer/components/TaskDrawerComponents/TaskChangesDrawer.vue
+++ task-management/src/main/webapp/vue-app/taskDrawer/components/TaskDrawerComponents/TaskChangesDrawer.vue
@@ -26,7 +26,7 @@
               v-for="(item, i) in logs"
               :key="i"
               class="pe-0">
-              <v-list-item-content class="pt-1">
+              <v-list-item-content class="mt-n1">
                 <div class="d-flex">
                   <exo-user-avatar
                     :username="item.author"
@@ -35,7 +35,7 @@
                     :size="30"
                     :url="null"
                     class="changeUserAvatar" />
-                  <p class="changesText mt-6 ps-1" v-html="renderChangeHTML(item)"></p>
+                  <div class="d-flex" v-html="renderChangeHTML(item)"></div>
                 </div>
                 <div>
                   <div class="dateTime caption px-10 my-n3">
@@ -104,23 +104,19 @@ export default {
       let str = '';
       if ( item.actionName === 'assign' || item.actionName === 'unassign') {
         const targetFullName = item.isTargetFullNameExternal ? `${item.targetFullName} (${this.$t('label.external')})` :  `${item.targetFullName}`;
-        str = `<p class='text-truncate mb-3' title='${item.authorFullName} ${this.$t(this.logMsg(item))} ${targetFullName}'>` +
+        str = `<p class='text-truncate my-auto text-color ms-1 subtitle-2' title='${item.authorFullName} ${this.$t(this.logMsg(item))} ${targetFullName}'>` +
             `<span>${ this.$t(this.logMsg(item))}</span>`+
             `<a href='/portal/dw/profile/${item.target}'> ${targetFullName} </a>`+
             '</p>';
       } else if ( item.actionName === 'edit_project' ) {
-        str = `<p class='text-truncate mb-3' title='${item.authorFullName} ${this.$t(this.logMsg(item))}  ${item.target}'>` +
+        str = `<p class='text-truncate my-auto text-color ms-1 subtitle-2' title='${item.authorFullName} ${this.$t(this.logMsg(item))}  ${item.target}'>` +
             `<span>${ this.$t(this.logMsg(item))}</span>`+
             `<a href='#'> ${item.target} </a>`+
             '</p>';
       } else if ( item.actionName === 'edit_priority' ) {
-        str = '<p class=\'mb-0\'>' +
-            `<p class='text-truncate mb-3' title='${item.authorFullName} ${this.$t(this.logMsg(item))} ${item.task.status.priority}'> ${ this.$t(this.logMsg(item)) } ${item.task.priority}</p>`+
-          '</p>';
+        str = `<p class='text-truncate my-auto text-color ms-1 subtitle-2' title='${item.authorFullName} ${this.$t(this.logMsg(item))} ${item.task.status.priority}'> ${ this.$t(this.logMsg(item)) } ${item.task.priority}</p>`;
       } else {
-        str = '<p class=\'mb-0\'>' +
-            `<p class='text-truncate mb-3' title='${item.authorFullName} ${this.$t(this.logMsg(item))} ${item.target}'> ${ this.$t(this.logMsg(item)) } ${item.target}</p>`+
-          '</p>';
+        str = `<p class='text-truncate my-auto text-color ms-1 subtitle-2' title='${item.authorFullName} ${this.$t(this.logMsg(item))} ${item.target}'> ${ this.$t(this.logMsg(item)) } ${item.target}</p>`;
       }
       return str;
     },
