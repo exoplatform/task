f077d7401335cb9f47eb4cf011bb549d48ee8cb6
Mhamdi Wassim <wmhamdi@exoplatform.com>
2022-02-01 14:26:10 +0100

    Task-44573: Add the maximum number of characters counter for task description Problem: The task description field has no indication on the description's maximum number of characters Fix: we add the character conter to make a limitation of the task's descreption which is 2000 caracters like the one of project description field

--- task-management/src/main/webapp/vue-app/taskDrawer/components/TaskDrawerComponents/TaskDescriptionEditor.vue
+++ task-management/src/main/webapp/vue-app/taskDrawer/components/TaskDrawerComponents/TaskDescriptionEditor.vue
@@ -12,13 +12,15 @@
       v-html="inputVal ? urlVerify(inputVal) : inputVal">
       {{ placeholder }}
     </div>
-    <textarea
-      id="descriptionContent"
-      ref="editor"
+
+    <exo-task-editor
+      v-if="displayEditor"
+      ref="richEditor"
       v-model="inputVal"
-      class="d-none"
-      cols="30"
-      rows="10"></textarea>
+      :max-length="MESSAGE_MAX_LENGTH"
+      :id="task.id"
+      :placeholder="$t('task.placeholder').replace('{0}', MESSAGE_MAX_LENGTH)" />
+
   </div>
 </template>
 
@@ -46,13 +48,18 @@ export default {
   },
   data() {
     return {
+      MESSAGE_MAX_LENGTH: 2000,
       inputVal: this.value,
-      editorReady: false
+      editorReady: false,
+      showEditor: false,
     };
   },
   computed: {
     taskDescription () {
       return this.task && this.task.id && this.task.description || '';
+    },
+    displayEditor() {
+      return this.showEditor;
     }
   },
   watch: {
@@ -163,6 +170,7 @@ export default {
       });
     },
     showDescriptionEditor: function (event) {
+      this.showEditor = !this.showEditor;
       const target = $( event.target );
       if ( target.is( 'a' ) ) {
         const url = target[0].href;
